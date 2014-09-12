f05fa07213803aec64674b5bd6c5d1ddd8fb273c
immanuelw <immwa@sas.upenn.edu>
2014-09-12 00:18:15 -0400

    Created weapons script, soon to include classes

---
+++ weapons.py
@@ -0,0 +1,18 @@
+'''
+Py6V -- Pythonic VVVVVV
+weapons -- Various weapons
+
+Defines various weapons that may be used throughout the game.
+'''
+
+import time
+
+import pygame
+from pygame.locals import *
+
+from extrect import ExtRect
+from entity import *
+
+from config import *
+
+
